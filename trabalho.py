926f849ebf1923a94c3ed96f6a75a1fd63168782
rylkoraul <jp.pereira23564@gmail.com>
2024-10-16 10:07:56 -0300

    wip

---
+++ trabalho.py
@@ -0,0 +1,11 @@
+class Cliente():
+    def __init__(self, nome, cpf, data_nascimento, endereco, email, telefone, data_registro):
+       self.nome = nome
+       self.cpf = cpf
+       self.data_nascimento = data_nascimento
+       self.endereco = endereco
+       self.email = email
+       self.telefone = telefone
+       self.data_registro = data_registro
+
+ 
